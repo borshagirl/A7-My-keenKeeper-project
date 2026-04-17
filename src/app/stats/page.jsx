665ddb9e7e67d6aefdc42fe89b48803c930d70a3
borshagirl <borshasabikunbashar@gmail.com>
2026-04-17 15:54:56 +0600

    Appended pieChart

--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -1,12 +1,69 @@
+'use client'
 
-import React from 'react';
+import { useEffect, useState } from "react"
+import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts"
 
-const StatsPage = () => {
-    return (
-        <div>
-            <h2>Stats Page</h2>
+export default function StatsPage() {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const stored = JSON.parse(localStorage.getItem("timeline")) || []
+
+    const call = stored.filter(i => i.type === "Call").length
+    const text = stored.filter(i => i.type === "Text").length
+    const video = stored.filter(i => i.type === "Video").length
+
+    const chartData = [
+      { name: "Call", value: call },
+      { name: "Text", value: text },
+      { name: "Video", value: video }
+    ]
+
+    setData(chartData)
+  }, [])
+
+  const COLORS = ["#3b82f6", "#10b981", "#f59e0b"]
+
+  return (
+    <div className="w-[80vw] mx-auto mt-5">
+    <h2 className="mb-5 text-3xl font-bold">Friendship Analytics</h2>
+    <div className="p-2 space-y-2 shadow-lg">
+
+      <h1 className="text-gray-600 font-semibold">By Interaction Type</h1>
+
+      {data.every(d => d.value === 0) ? (
+        <p className="text-center text-gray-400">
+          No data available
+        </p>
+      ) : (
+        <div className="flex justify-center">
+          <PieChart width={300} height={300}>
+            
+            <Pie
+              data={data}
+              dataKey="value"
+              nameKey="name"
+              cx="50%"
+              cy="50%"
+              outerRadius={120}
+              innerRadius={90}
+              paddingAngle={5}
+              label
+            >
+              {data.map((entry, index) => (
+                <Cell key={index} fill={COLORS[index]} />
+              ))}
+            </Pie>
+
+            <Tooltip />
+            <Legend />
+
+          </PieChart>
         </div>
-    );
-};
+      )}
 
-export default StatsPage;
+    </div>
+    </div>
+  )
+}
